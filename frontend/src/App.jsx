523205803faf68938ff32949082555bf61e4009e
Harsh Shukla <harshshuklaa1104@gmail.com>
2026-03-29 16:02:30 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,14 @@ export default function App() {
   const name = params.get('name') || 'Anonymous';
   const room = params.get('room') || 'default';
 
+  const wsUrl =
+    window.location.hostname === "localhost"
+      ? "ws://localhost:3001"                    
+      : "wss://codesync-1zeu.onrender.com/";        
+
   return (
     <CollabEditorYjs
-      wsUrl="ws://localhost:3001"
+      wsUrl={wsUrl}
       roomId={room}
       userName={name}
       userColor="#8B5CF6"
